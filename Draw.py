e64815ae6972281ce67591b9cf26acb6630fdf24
SevenLeaves <2561056905@qq.com>
2024-07-02 01:09:33 +0800

    add bar map view api

--- Draw.py
+++ Draw.py
@@ -1,10 +1,10 @@
 import webbrowser
 import findspark
-from pyecharts.charts import Line,Bar
+from pyecharts.charts import Line, Bar, Map
 from pyecharts import options as opts
 from pyspark.sql import SparkSession
 from Temtable import createtable
-def drawline(title,x_data, y_data,y_Data2):
+def drawline(title,x_data, y_data,y_data2):
     line = (Line(init_opts=opts.InitOpts(width="1600px", height="850px")).add_xaxis(x_data)
             .add_yaxis("疫情新增病例",y_data)
             .add_yaxis("疫情死亡病例",y_data2)
@@ -31,6 +31,87 @@ def drawbar(title,x_data,y_data,y_data2):
          .render("bar.html")
          )
     webbrowser.open("bar.html")
+def drawmap(title,data):
+    NAME_MAP_DATA_US = {
+        "Alabama": "Alabama",
+        "Alaska": "Alaska",
+        "Arizona": "Arizona",
+        "Arkansas": "Arkansas",
+        "California": "California",
+        "Colorado": "Colorado",
+        "Connecticut": "Connecticut",
+        "Delaware": "Delaware",
+        "Florida": "Florida",
+        "Georgia": "Georgia",
+        "Hawaii": "Hawaii",
+        "Idaho": "Idaho",
+        "Illinois": "Illinois",
+        "Indiana": "Indiana",
+        "Iowa": "Iowa",
+        "Kansas": "Kansas",
+        "Kentucky": "Kentucky",
+        "Louisiana": "Louisiana",
+        "Maine": "Maine",
+        "Maryland": "Maryland",
+        "Massachusetts": "Massachusetts",
+        "Michigan": "Michigan",
+        "Minnesota": "Minnesota",
+        "Mississippi": "Mississippi",
+        "Missouri": "Missouri",
+        "Montana": "Montana",
+        "Nebraska": "Nebraska",
+        "Nevada": "Nevada",
+        "New Hampshire": "New Hampshire",
+        "New Jersey": "New Jersey",
+        "New Mexico": "New Mexico",
+        "New York": "New York",
+        "North Carolina": "North Carolina",
+        "North Dakota": "North Dakota",
+        "Ohio": "Ohio",
+        "Oklahoma": "Oklahoma",
+        "Oregon": "Oregon",
+        "Pennsylvania": "Pennsylvania",
+        "Rhode Island": "Rhode Island",
+        "South Carolina": "South Carolina",
+        "South Dakota": "South Dakota",
+        "Tennessee": "Tennessee",
+        "Texas": "Texas",
+        "Utah": "Utah",
+        "Vermont": "Vermont",
+        "Virginia": "Virginia",
+        "Washington": "Washington",
+        "West Virginia": "West Virginia",
+        "Wisconsin": "Wisconsin",
+        "Wyoming": "Wyoming"
+    }
+    map=(Map(init_opts=opts.InitOpts(width="1600px", height="850px"))
+    .add(
+        series_name="美国各州累计新增病例图",
+        maptype="美国",
+        data_pair=data,
+        name_map=NAME_MAP_DATA_US,
+        is_map_symbol_show=False,
+    )
+    .set_global_opts(
+        title_opts=opts.TitleOpts(
+            title="美国各州累计新增病例图",
+            subtitle="数据集来自kaggle",
+        ),
+        tooltip_opts=opts.TooltipOpts(
+            trigger="item", formatter="{b}<br/>{c} cases"
+        ),
+        visualmap_opts=opts.VisualMapOpts(
+            min_=800,
+            max_=3000000000,
+            range_text=["High", "Low"],
+            is_calculable=True,
+            range_color=["lightskyblue", "yellow", "orangered"],
+        ),
+    )
+    .render("map.html")
+    )
+    webbrowser.open("map.html")
+
 # 初始化 findspark 和 SparkSession
 findspark.init()
 spark = SparkSession.builder.appName("BigData").getOrCreate()
@@ -52,3 +133,7 @@ x_data=[row['state'] for row in bardata.collect()]
 y_data=[row['cases'] for row in bardata.collect()]
 y_data2=[row['deaths'] for row in bardata.collect()]
 drawbar('美国各州疫情柱状图',x_data,y_data,y_data2)
+
+mapdata=spark.sql("select state,sum(cases) cases from table group by state order by cases")
+mpdata=mapdata.collect()
+drawmap("图",mpdata)
--- map.py
+++
@@ -1,9 +0,0 @@
-from pyecharts import options as opts
-from pyecharts.charts import Map
-from pyecharts.faker import Faker
-c = (
-    Map()
-    .add([1,2],"china")
-    .set_global_opts(title_opts=opts.TitleOpts(title="Map-基本示例"))
-    .render("map_base.html")
-)
