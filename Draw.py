9f7db6c1056fcfbebce174cadd8c1c32e307ee41
SevenLeaves <2561056905@qq.com>
2024-07-01 16:34:18 +0800

    draw date - cases deaths Line view

--- Draw.py
+++ Draw.py
@@ -1,14 +1,14 @@
 import webbrowser
 import findspark
-from pyecharts.charts import Line
+from pyecharts.charts import Line,Bar
 from pyecharts import options as opts
 from pyspark.sql import SparkSession
 from Temtable import createtable
-def drawline(x_data, y_data,y_Data2):
+def drawline(title,x_data, y_data,y_Data2):
     line = (Line().add_xaxis(x_data)
             .add_yaxis("疫情新增病例",y_data)
             .add_yaxis("疫情死亡病例",y_data2)
-            .set_global_opts(title_opts=opts.TitleOpts(title="美国疫情图", subtitle="数据来自kaggle"),
+            .set_global_opts(title_opts=opts.TitleOpts(title=title, subtitle="数据来自kaggle"),
                 datazoom_opts=[
                     opts.DataZoomOpts(range_start=0, range_end=100),
                     opts.DataZoomOpts(type_="inside", range_start=0, range_end=100),
@@ -16,6 +16,9 @@ def drawline(x_data, y_data,y_Data2):
             )
             .render("line_echart.html"))
     webbrowser.open("line_echart.html")
+def drawbar(title,x_data,y_data):
+    bar=(Bar().add_xaxis(x_data)
+         .add_yaxis("疫情新增病例",y_data))
 # 初始化 findspark 和 SparkSession
 findspark.init()
 spark = SparkSession.builder.appName("BigData").getOrCreate()
@@ -23,11 +26,11 @@ spark = SparkSession.builder.appName("BigData").getOrCreate()
 # 创建数据表
 createtable()
 # 从表中选择数据并展示
-data = spark.sql("SELECT date, cases,deaths FROM table WHERE fips=17031 ORDER BY date ASC")
+data = spark.sql("select date,sum(cases) cases,sum(deaths) deaths from table where state='California' group by date order by date asc")
 data.show()
 
 # 提取数据到列表
 x_data = [row['date'] for row in data.collect()]
 y_data = [row['cases'] for row in data.collect()]
 y_data2= [row['deaths'] for row in data.collect()]
-drawline(x_data,y_data,y_data2)
+drawline("加州疫情图",x_data,y_data,y_data2)
