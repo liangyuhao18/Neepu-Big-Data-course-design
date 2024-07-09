ae28997786c31fa7394c6c0799f5f2325d77b683
liangyuhao <liangyuhao101@foxmail.com>
2024-07-09 15:37:17 +0800

    final

--- bstest.py
+++ bstest.py
@@ -7,25 +7,15 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 script_tag = soup.find('script', type='application/ld+json')
 
-if script_tag:
-    json_content = script_tag.string
-    data = json.loads(json_content)
-    # 提取contentUrl
-    content_urls = [distribution['contentUrl'] for distribution in data['distribution'] if 'contentUrl' in distribution]
-    if content_urls:
-        file_url = content_urls[0]
-        print(f"Downloading from: {file_url}")
+json_content = script_tag.string
+data = json.loads(json_content)
+# 提取contentUrl
+content_urls = [distribution['contentUrl'] for distribution in data['distribution'] if 'contentUrl' in distribution]
+print(content_urls)
+response02 = requests.get(content_urls[0])
+with open('response.txt', 'w') as file:
+    # 要写入文件的内容
+    content = str(response02.headers)
 
-        # 下载文件
-        response = requests.get(file_url)
-        if response.status_code == 200:
-            # 保存文件
-            with open('dataset.zip', 'wb') as file:
-                file.write(response.content)
-            print("File downloaded successfully.")
-        else:
-            print("Failed to download file.")
-    else:
-        print("No contentUrl found.")
-else:
-    print("No JSON-LD script tag found.")
+    # 将内容写入文件
+    file.write(content)
